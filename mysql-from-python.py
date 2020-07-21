e9f68fdf755314b1768378fdc90fe1e101560017
vaida898 <freelpfm@gmail.com>
2020-07-21 19:40:25 +0000

    added new variable to friends table

--- mysql-from-python.py
+++ mysql-from-python.py
@@ -15,11 +15,9 @@ connection = pymysql.connect(host='localhost',
 try:
     # Run the query
     with connection.cursor() as cursor:
-        cursor.execute("""CREATE TABLE IF NOT EXISTS 
-                        Friends(name char(20), age int, DOB datetime);""")
-        # Note that the above will still display a warning (not error) if the 
-        # table already exists
-
+        row = ("Bob", 21, "1990-02-06 23:04:06")
+        cursor.execute("INSERT INTO Friends VALUES (%s, %s, %s);", row)
+        connection.commit()
 
 finally:
     # Close the connection, regardless of wether the above was successful
